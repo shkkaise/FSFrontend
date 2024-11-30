3ab41decd0e882ebe1c5890e42dbf02d31dda9f5
shkkaise <123665233+shkkaise@users.noreply.github.com>
2024-11-30 10:10:35 +0000

    AA

---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
+
+const LoginSignup = () => {
+
+  const [state,setState] = useState("Login");
+  const [formData,setFormData] = useState({username:"",email:"",password:""});
+
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value});
+    }
+
+  const login = async () => {
+    let dataObj;
+    await fetch('https://symmetrical-palm-tree-pvq649w6wwpf7xgv-3000.app.github.dev/login', {
+      method: 'POST',
+      headers: {
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {dataObj=data});
+      console.log(dataObj);
+      if (dataObj.success) {
+        localStorage.setItem('auth-token',dataObj.token);
+        window.location.replace("/");
+      }
+      else
+      {
+        alert(dataObj.errors)
+      }
+  }
+
+  const signup = async () => {
+    let dataObj;
+    await fetch('https://symmetrical-palm-tree-pvq649w6wwpf7xgv-3000.app.github.dev/signup', {
+      method: 'POST',
+      headers: {
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {dataObj=data});
+
+      if (dataObj.success) {
+        localStorage.setItem('auth-token',dataObj.token);
+        window.location.replace("/");
+      }
+      else
+      {
+        alert(dataObj.errors)
+      }
+  }
+
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>{state}</h1>
+        <div className="loginsignup-fields">
+          {state==="Sign Up"?<input type="text" placeholder="Your name" name="username" value={formData.username} onChange={changeHandler}/>:<></>}
+          <input type="email" placeholder="Email address" name="email" value={formData.email} onChange={changeHandler}/>
+          <input type="password" placeholder="Password" name="password" value={formData.password} onChange={changeHandler}/>
+          <input type="text" placeholder="Address" name="add1" value={formData.add1} onChange={changeHandler}/>
+          <input type="text" placeholder="Address " name="add2" value={formData.add2} onChange={changeHandler}/>
+          <input type="Number" placeholder="PinCode" name="Pincode" value={formData.pincode} onChange={changeHandler}/>
+          <input type="City" placeholder="City" name="City" value={formData.city} onChange={changeHandler}/>
+          <input type="State" placeholder="State" name="State" value={formData.state} onChange={changeHandler}/>
+        </div>
+
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+
+        {state==="Login"?
+        <p className="loginsignup-login">Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
+        :<p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>}
+
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+
+const Shop = () => {
+
+  const [popular, setPopular] = useState([]);
+  const [newcollection, setNewCollection] = useState([]);
+
+  const fetchInfo = () => { 
+    fetch('https://symmetrical-palm-tree-pvq649w6wwpf7xgv-3000.app.github.dev/popularinwomen') 
+            .then((res) => res.json()) 
+            .then((data) => setPopular(data))
+    fetch('https://symmetrical-palm-tree-pvq649w6wwpf7xgv-3000.app.github.dev/newcollections') 
+            .then((res) => res.json()) 
+            .then((data) => setNewCollection(data))
+    }
+
+    useEffect(() => {
+      fetchInfo();
+    }, [])
+
+
+  return (
+    <div>
+      <Hero/>
+      <Popular data={popular}/>
+      <Offers/>
+      <NewCollections data={newcollection}/>
+      <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
